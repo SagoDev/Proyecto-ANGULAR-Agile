6350100dedbd3679433de5279fba708d42a24520
Santiago Lanz <Santiagolanz019@gmail.com>
2024-07-08 14:47:59 -0300

    Dashboard  Structure
    - Added initial view to dashboard
      component.
    - Added bootstrap.min.css to angular.json.
    - Added some css to general style to clean
      margins and padding.

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SideMenuComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- client/src/app/pages/dashboard/dashboard.routes.ts
+++ client/src/app/pages/dashboard/dashboard.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const DASHBOARD_ROUTES: Routes = [
     {
-        path: 'dashboard',
+        path: '',
         loadComponent: () => import('./dashboard.component'),
         children: [
             {
