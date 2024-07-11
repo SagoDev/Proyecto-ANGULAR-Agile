3341e9963c4061cc310c01e72f33b60ba453ea9a
Santiago Lanz <Santiagolanz019@gmail.com>
2024-07-11 19:01:26 -0300

    Side Menu -  Routes Config
    - Configurated routes to each option  of the side menu.

--- client/src/app/pages/dashboard/components/side-menu/side-menu.component.ts
+++ client/src/app/pages/dashboard/components/side-menu/side-menu.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { DASHBOARD_ROUTES } from '../../dashboard.routes';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
-export class SideMenuComponent {
-
+export class SideMenuComponent {  
+  public dashboardOptions = DASHBOARD_ROUTES.map(r => r.children ?? []).flat();
+ 
 }
