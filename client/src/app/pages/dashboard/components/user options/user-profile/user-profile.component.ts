f1a1a8aadf3017a8f953ce0eb78e84929948c610
Santiago Lanz <Santiagolanz019@gmail.com>
2024-07-15 18:53:27 -0300

    Profile User Structure/Style
    - Added initial view for user profile
    component.
    - Added some css style to the component.

--- client/src/app/pages/dashboard/components/user options/user-profile/user-profile.component.ts
+++ client/src/app/pages/dashboard/components/user options/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
