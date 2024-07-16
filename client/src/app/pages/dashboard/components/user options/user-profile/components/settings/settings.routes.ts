d89d86bc2447e66f4018d4f198bdfa5768803a23
Santiago Lanz <Santiagolanz019@gmail.com>
2024-07-16 16:01:25 -0300

    Deleted  App Settings Component

--- client/src/app/pages/dashboard/components/user options/user-profile/components/settings/settings.routes.ts
+++ client/src/app/pages/dashboard/components/user options/user-profile/components/settings/settings.routes.ts
@@ -16,12 +16,7 @@ export const USER_SETTINGS_ROUTES: Routes = [
         path: 'Security',
         loadComponent: () => import('./security-settings/security-settings.component'),
         title: 'Security'
-    },
-    {
-        path: 'Application',
-        loadComponent: () => import('./application-settings/application-settings.component'),
-        title: 'Application'
-    },
+    },   
     {
         path: 'Notification',
         loadComponent: () => import('./notification-settings/notification-settings.component'),
